5252fbce71a77255721c3c17702c5a4af989c63b
Mabel <mabelmdowell@gmail.com>
2023-10-25 10:32:58 -0400

    Fixed encoded function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #lab 6: Version Control
 #Date: 10/25/2023
 
+#displays menu
 def show_menu():
     print('Menu')
     print('-------------')
@@ -10,10 +11,16 @@ def show_menu():
     print('3. Quit')
     print()
 
+#encodes password
 def encode(password):
     encoded_pass = ''
+    #iterates through password and adds three
     for i in password:
-        encoded_char = str(int(i) + 3)
+        encoded_char = int(i) + 3
+        #account for if the number adds up to be more than 10
+        if encoded_char >= 10:
+            encoded_char -= 10
+        encoded_char = str(encoded_char)
         encoded_pass += encoded_char
     return encoded_pass
 
@@ -28,7 +35,9 @@ def decode(password):
         new_password = new_password + char
     return new_password
 
+#main function
 def main():
+    #if the user wants to see menu and wants to continue asking the programmer to encode/ decode
     display_menu = True
     user_continue = True
 
@@ -36,6 +45,7 @@ def main():
         if display_menu:
             show_menu()
 
+        #ask user what option they want and perform necessary functions
         user_input = int(input('Please enter an option: '))
         if user_input == 1:
             password_to_encode = input('Please enter your password to encode: ')
@@ -44,6 +54,7 @@ def main():
             print()
         elif user_input == 2:
             decoded_password = decode(encoded_password)
+            print(encoded_password)
             print(decoded_password)
         elif user_input == 3:
             display_menu = False
