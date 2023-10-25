6aaef5affa24c172af56fe4c15477f504939753a
oickle <oycell@aol.com>
2023-10-25 10:16:27 -0400

    Add basic decode functionality

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 #lab 6: Version Control
 #Date: 10/25/2023
 
-
 def show_menu():
     print('Menu')
     print('-------------')
@@ -18,6 +17,17 @@ def encode(password):
         encoded_pass += encoded_char
     return encoded_pass
 
+def decode(password):
+    new_password = ''
+    for i in password:
+        num = int(i) - 3
+        if num < 0:
+            char = str(9 + (num + 1))
+        else:
+            char = str(num)
+        new_password = new_password + char
+    return new_password
+
 def main():
     display_menu = True
     user_continue = True
@@ -32,6 +42,9 @@ def main():
             encoded_password = encode(password_to_encode)
             print('Your password has been encoded and stored!')
             print()
+        elif user_input == 2:
+            decoded_password = decode(encoded_password)
+            print(decoded_password)
         elif user_input == 3:
             display_menu = False
             user_continue = False
